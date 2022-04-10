39a2e3b19d94c9680eaf83e652a532454a20f1e9
Nam Anh Mai <maithenamanh@gmail.com>
2022-04-10 11:10:26 -0700

    Added function to play multiple rounds

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
 function computerPlay() {
     let choice = ['Rock', 'Paper', 'Scissors'];
     let computerSelection = choice[Math.floor(Math.random() * choice.length)];
-    console.log(`Computer: ${computerSelection}`);
+    // console.log(`Computer: ${computerSelection}`);
     return computerSelection;
 }
 
 function playRound(playerSelection, computerSelection) {
-    let input = playerSelection.toLowerCase();
-    let firstChar = input.charAt(0).toUpperCase();
-    input = firstChar.concat(input.substring(1));
-    console.log(`Player: ${input}`);
+    let firstChar = playerSelection.charAt(0).toUpperCase();
+    playerSelection = firstChar.concat(playerSelection.substring(1));
+    // console.log(`Player: ${input}`);
 
     let result = "Tie Game";
 
-    if (input == computerSelection) {
+    if (playerSelection == computerSelection) {
         //pass
-    } else if (input == 'Rock' && computerSelection == 'Scissors'
-        || input == 'Paper' && computerSelection == 'Rock'
-        || input == 'Scissors' && computerSelection == 'Paper'
+    } else if (playerSelection == 'Rock' && computerSelection == 'Scissors'
+        || playerSelection == 'Paper' && computerSelection == 'Rock'
+        || playerSelection == 'Scissors' && computerSelection == 'Paper'
         ) {
-            result = `You Win!`;
+            result = `You Win! ${playerSelection} beats ${computerSelection}`;
     }
     else
-        result = `You Lose! ${computerSelection} beats ${input}`;
+        result = `You Lose! ${computerSelection} beats ${playerSelection}`;
 
     return result;
 }
 
+function game() {
+    for (let i = 0; i < 1; i++) {
+        let playerSelection = prompt("What's your hand?");
+        let computerSelection = computerPlay()
+        let result = playRound(playerSelection, computerSelection);
+        console.log(`Round ${i + 1}: ${result}`);
+    }
+}
+
+/* First round game */
+
 // Player can enter case-insensitive option
-const playerSelection = 'RoCK';
+// const playerSelection = 'RoCK';
+// const computerSelection = computerPlay();
+// console.log(playRound(playerSelection, computerSelection));
 
-const computerSelection = computerPlay();
-
-console.log(playRound(playerSelection, computerSelection));
+/* Five round game */
+game();
